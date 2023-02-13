d973ad69df60f37b9a470fe971edef6dde8621c4
Joseph <yousef.aburizek@gmail.com>
2023-02-13 22:50:09 +0200

    MySQL connected

--- flight_project/settings.py
+++ flight_project/settings.py
@@ -88,6 +88,14 @@ WSGI_APPLICATION = 'flight_project.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'flight_project',
+        'USER': 'root',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
+    },
+    'db_sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
