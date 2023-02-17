36458f6ab4639414dc5bee2216ef8116ddef09e3
Joseph <yousef.aburizek@gmail.com>
2023-02-18 00:12:58 +0200

    Flight & Ticket models added

--- flights/models.py
+++ flights/models.py
@@ -5,3 +5,14 @@ from django.core.validators import MinLengthValidator
 class Country(models.Model):
     name = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
     image = models.ImageField(blank=True, null=True, default='', upload_to='countries/')
+
+    def __str__(self):
+        return f'{self.name}'
+    
+
+class Flight(models.Model):
+    pass
+
+
+class Ticket(models.Model):
+    pass
